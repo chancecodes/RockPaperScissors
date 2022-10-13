1d6325a212ce296eaeb57cb712434f5af0b8bbef
chancecodes <chanceycoding@gmail.com>
2022-10-13 16:42:30 -0700

    play best of 5 with logged scores

--- javascript.js
+++ javascript.js
@@ -12,23 +12,52 @@ function getComputerChoice() {
     return choices[randomChoice];
 }
 
+// const computerSelection = getComputerChoice();
+// const playerSelection = prompt("Pick!", "Rock, Paper, or Scissors").toLowerCase();
 
-
-let computerSelection = getComputerChoice();
-let playerSelection = prompt("Pick!", "Rock, Paper, or Scissors").toLowerCase();
-
-
-if (playerSelection == computerSelection) {
-    alert ("Tie!")
-}  else if (playerSelection == "rock" && computerSelection == "scissors") {
-    alert ("You win")
-}  else if (playerSelection == "paper" && computerSelection == "rock") {
-    alert ("You win")
-}  else if (playerSelection == "scissors" && computerSelection == "paper") {
-    alert ("You win")
-}   else {
-    alert ("You lose")
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return ("Tie!")
+    }  else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return ("You win")
+    }  else if (playerSelection == "paper" && computerSelection == "rock") {
+        return ("You win")
+    }  else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return ("You win")
+    }   else {
+        return ("You lose")
+    }
 }
 
-console.log(computerSelection)
-console.log(playerSelection)
+
+
+
+// play a round
+// play again if tie 
+// tally score 
+// repeat until 5 games are played
+
+function game() {
+    let userScore = 0;
+    let computerScore = 0;
+    for(let i = 0; i < 5; i++) {
+        var playerSelection = prompt("Pick!", "Rock, Paper, or Scissors").toLowerCase();
+        var computerSelection = getComputerChoice();
+        console.log(playerSelection)
+        console.log(computerSelection)
+        console.log(playRound(playerSelection, computerSelection))
+        if (playRound(playerSelection,computerSelection) == "Tie!") {
+            alert ("Tie! play again")
+            i = i - 1
+        } else if (playRound(playerSelection, computerSelection) == "You win") {
+            userScore = userScore + 1
+            alert ("Nice! Score is " + userScore + " to " + computerScore)
+        } else {
+            computerScore = computerScore + 1
+            alert ("Yikes! Score is " + userScore + " to " + computerScore)
+        }
+    }
+    console.log(userScore + " to " + computerScore)
+}
+
+console.log(game())
