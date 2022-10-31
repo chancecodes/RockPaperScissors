552bb4fa3a5cceefe7b5c596f876dc32780f4e78
chancecodes <chanceycoding@gmail.com>
2022-10-31 16:28:51 -0700

    Add buttons and initiate playRound on click

--- javascript.js
+++ javascript.js
@@ -17,15 +17,15 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        return ("Tie!")
+        console.log("Tie!")
     }  else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return ("You win")
+        console.log ("You win")
     }  else if (playerSelection == "paper" && computerSelection == "rock") {
-        return ("You win")
+        console.log ("You win")
     }  else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return ("You win")
+        console.log ("You win")
     }   else {
-        return ("You lose")
+        console.log ("You lose")
     }
 }
 
@@ -37,27 +37,49 @@ function playRound(playerSelection, computerSelection) {
 // tally score 
 // repeat until 5 games are played
 
-function game() {
-    let userScore = 0;
-    let computerScore = 0;
-    for(let i = 0; i < 5; i++) {
-        var playerSelection = prompt("Pick!", "Rock, Paper, or Scissors").toLowerCase();
+// function game() {
+//     let userScore = 0;
+//     let computerScore = 0;
+//     for(let i = 0; i < 5; i++) {
+//         var playerSelection = prompt("Pick!", "Rock, Paper, or Scissors").toLowerCase();
+//         var computerSelection = getComputerChoice();
+//         console.log(playerSelection)
+//         console.log(computerSelection)
+//         console.log(playRound(playerSelection, computerSelection))
+//         if (playRound(playerSelection,computerSelection) == "Tie!") {
+//             alert ("Tie! play again");
+//             i -= 1;
+//         } else if (playRound(playerSelection, computerSelection) == "You win") {
+//             userScore += 1;
+//             alert ("Nice! Score is " + userScore + " to " + computerScore);
+//         } else {
+//             computerScore += 1;
+//             alert ("Yikes! Score is " + userScore + " to " + computerScore);
+//         }
+//     }
+//     console.log(userScore + " to " + computerScore);
+// }
+
+
+const btn = document.querySelectorAll(".btn");
+btn.forEach((btn) => {
+    btn.addEventListener ('click', function(e) {
+        var playerSelection = e.target.innerHTML;
         var computerSelection = getComputerChoice();
+        
         console.log(playerSelection)
         console.log(computerSelection)
-        console.log(playRound(playerSelection, computerSelection))
-        if (playRound(playerSelection,computerSelection) == "Tie!") {
-            alert ("Tie! play again")
-            i = i - 1
-        } else if (playRound(playerSelection, computerSelection) == "You win") {
-            userScore = userScore + 1
-            alert ("Nice! Score is " + userScore + " to " + computerScore)
-        } else {
-            computerScore = computerScore + 1
-            alert ("Yikes! Score is " + userScore + " to " + computerScore)
-        }
-    }
-    console.log(userScore + " to " + computerScore)
-}
 
-console.log(game())
+
+        playRound(playerSelection,computerSelection);
+
+    })
+})
+
+//create a function that appendschild everytime playround is run
+
+// const container = document.querySelector('#container');
+// const results = document.createElement('div');
+
+// results.textContent = "WIN OR LOSE"
+// container.appendChild(results)
